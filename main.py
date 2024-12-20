f8c7e770cb50b57a0d5de0bb79e6b830f8e47963
Feiyang Wu <feiyangw.21@intl.zju.edu.cn>
2024-12-20 15:36:04 +0800

    mujoco setup

---
+++ main.py
@@ -0,0 +1,8 @@
+import mujoco_gym_app
+
+def main():
+	mjApp = MuJocoGymApp(MujocoEnv, ABC)
+    pass
+    
+if __name__ == "__main__":
+    main()
---
+++ test_mujoco.py
@@ -0,0 +1,41 @@
+import time
+
+import mujoco
+import mujoco.viewer
+from gymnasium.envs.mujoco.mujoco_env import MujocoEnv
+
+m = mujoco.MjModel.from_xml_path('./models/arx5.xml')
+d = mujoco.MjData(m)
+
+print(m.nv)
+
+
+with mujoco.viewer.launch_passive(m, d) as viewer:
+  # Close the viewer automatically after 30 wall-seconds.
+  while viewer.is_running():
+    step_start = time.time()
+
+    # mj_step can be replaced with code that also evaluates
+    # a policy and applies a control signal before stepping the physics.
+    mujoco.mj_step(m, d)
+
+    # Example modification of a viewer option: toggle contact points every two seconds.
+    # with viewer.lock():
+    #   viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = int(d.time % 2)
+
+    # Pick up changes to the physics state, apply perturbations, update options from GUI.
+    viewer.sync()
+
+
+    d.qpos[0] = 0.5
+    d.qpos[1] = 0.5
+    d.qpos[2] = 0.5
+    d.qpos[4] = 0.3
+    d.qpos[3] = 0.4
+    d.qpos[5] = 0.15
+    
+
+    # Rudimentary time keeping, will drift relative to wall clock.
+    time_until_next_step = m.opt.timestep - (time.time() - step_start)
+    if time_until_next_step > 0:
+      time.sleep(time_until_next_step)
